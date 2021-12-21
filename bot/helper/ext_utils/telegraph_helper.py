066a2b987d5c331bfafcd0e1f5f50205aedab5a3
jitudas1 <96472009+jitudas1@users.noreply.github.com>
2021-12-22 00:51:58 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('Torrent/Leech downloaded', 'https://t.me/torrent_drive1')
