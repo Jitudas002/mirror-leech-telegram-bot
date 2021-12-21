49897a8b23f8d5f83afbb530176fa5b7a0fe58c3
jitudas1 <96472009+jitudas1@users.noreply.github.com>
2021-12-22 01:02:23 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,9 +1,9 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = ''
-        self.UnzipMirrorCommand = ''
-        self.ZipMirrorCommand = ''
+        self.MirrorCommand = '👇🎂🍿😒☠️🍿'
+        self.UnzipMirrorCommand = '☠️☠️☠️☠️'
+        self.ZipMirrorCommand = '☠️☠️☠️'
         self.CancelMirror = 'cancel'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
